5a06149fb0072f4edbbc998729eab815807683db
Ankit-euphemism <ankitkumarb47@bbdu.ac.in>
2026-04-08 18:00:56 +0530

    files update

--- Happenin/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Happenin</h1>
-    </>
-  );
-}
-
-export default App;
--- Happenin/src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY as string;
-
-// You can pass a generic type to createClient if you generate Supabase types
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
